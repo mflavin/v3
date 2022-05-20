378bf5749129fbb47b81bfb8d438a8696b27c26c
mflavin <mflavin@hawk.iit.edu>
2022-05-19 22:35:29 -0500

    Add more icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,21 @@
+import IconBold from './IconBold.vue';
+import IconCancel from './IconCancel.vue';
+import IconImage from './IconImage.vue';
+import IconItalic from './IconItalic.vue';
+import IconLink from './IconLink.vue';
+import IconPencil from './IconPencil.vue';
+import IconSave from './IconSave.vue';
+import IconTrash from './IconTrash.vue';
+import IconUnderline from './IconUnderline.vue';
+
+export {
+  IconBold,
+  IconCancel,
+  IconImage,
+  IconItalic,
+  IconLink,
+  IconPencil,
+  IconTrash,
+  IconSave,
+  IconUnderline,
+};
